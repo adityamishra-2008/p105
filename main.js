91293e16bc41759cf599d5a38977d135becab6cc
adityamishra-2008 <86653579+adityamishra-2008@users.noreply.github.com>
2022-05-30 16:57:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+Webcam.set(
+    { 
+        width:350, 
+        height:300, 
+        image_format : 'png',
+         png_quality:90 
+         });
+ camera = document.getElementById("camera"); 
+ Webcam.attach( '#camera' );
+
+ function take_snapshot()
+  { 
+      Webcam.snap(function(data_uri)
+   {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>'; }); 
+  }
+
+  console.log('ml5 version:', ml5.version);
+
+  classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/eMIBly3-z/model.json',modelLoaded);
+
+function modelLoaded()
+ {
+     console.log('Model Loaded!');
+ }
+
+function check()
+{
+    img = document.getElementById("captured_image");
+    classifier.classify(img,gotresult);
+}
+
+function gotresult(error,results)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    else
+    {
+       console.log(results);
+       document.getElementById("resut_ob_name").innerHTML= results[0].label;
+       document.getElementById("resut_ob_accuracy").innerHTML = results[0].confidence.toFixed(2);
+    }
+}
